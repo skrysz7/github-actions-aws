32bd3cd7c9485115ea8b9fc4192a04b8934a6123
awx <99257149+skrysz7@users.noreply.github.com>
2024-04-03 22:03:32 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = ${{ secrets.AWS_BACKEND_BUCKET }}
-    key            = ${{ secrets.AWS_BACKEND_KEY }}
-    region         = ${{ secrets.AWS_REGION }}
-    dynamodb_table = ${{ secrets.AWS_DYNAMODB_TABLE }}
+    bucket         = ${{ secrets.{AWS_BACKEND_BUCKET} }}
+    key            = ${{ secrets.{AWS_BACKEND_KEY} }}
+    region         = ${{ secrets.{AWS_REGION} }}
+    dynamodb_table = ${{ secrets.{AWS_DYNAMODB_TABLE} }}
     }
   }
