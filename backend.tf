d6a4c1b9e48a4f108b844fc1942b1f5c478f6ec8
awx <99257149+skrysz7@users.noreply.github.com>
2024-04-03 22:07:06 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = ${{ secrets.{AWS_BACKEND_BUCKET} }}
-    key            = ${{ secrets.{AWS_BACKEND_KEY} }}
-    region         = ${{ secrets.{AWS_REGION} }}
-    dynamodb_table = ${{ secrets.{AWS_DYNAMODB_TABLE} }}
+    bucket         = "skrysz7-terraform-state-file"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "skrysz7-tf-lockid"
     }
   }
