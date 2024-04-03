a74dfffea8af900da7d823f846b2a305d3ae4aaf
awx <99257149+skrysz7@users.noreply.github.com>
2024-04-03 21:46:58 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,5 +1,5 @@
 terraform {
-  backend = "s3" {
+  backend "s3" {
     bucket         = ${{ secrets.AWS_BACKEND_BUCKET }}
     key            = ${{ secrets.AWS_BACKEND_KEY }}
     region         = ${{ secrets.AWS_REGION }}
